af79e7d0d5de1ce319df8116ae2de9367239a556
Martijn de Boer <github@sexybiggetje.nl>
2019-01-15 21:21:34 +0100

    First iteration of GraphQL API for #2

--- app.js
+++ app.js
@@ -7,30 +7,85 @@ var visitor = ua('UA-2158627-15');
 var fs = require('fs');
 var rita = require('rita');
 var testNLP = false;
+var rms;
 
-/*var og = []; var oga = []; var ogt = [];
-var bg = []; var bga = []; var bgt = [];*/
+var { graphql, buildSchema } = require( 'graphql' );
+var gqlSchema = buildSchema( fs.readFileSync( './htdocs/schema.gql', 'utf8' ) );
+var gqlImpl = {
+    random: function () {
+
+        var index = Math.floor(Math.random() * (quotes.length)) ;
+
+        visitor.pageview("/graphql/quote/" + index).send();
+        visitor.event("Return Quote", "Random Quote").send()
+
+        var quote = flattenQuote( getQuote(index) );
+
+        return quote;
+    },
+    video: function () {
+        var theObject = {
+            "title": null,
+            "link": null
+        };
+
+        var index;
+        var theQuote;
+
+        while(theObject.title === null) {
+            index = Math.floor(Math.random() * (quotes.length)) ;
+            theQuote = getQuote(index);
+
+            if (theQuote.quote.videos && theQuote.quote.videos.length > 0) {
+                var video = theQuote.quote.videos[Math.floor(Math.random() * (theQuote.quote.videos.length))];
+                theObject.title = video.title;
+                theObject.link = video.link;
+            }
+        }
+
+        visitor.pageview("/json/random/video/" + index).send();
+        visitor.event("Return Video", "Random Video").send()
+
+        return theObject;
+    },
+    daily: function () {
+        var dat = new Date();
+        var index = parseInt(dat.getFullYear() + dat.getMonth() + dat.getDay()) % quotes.length;
+
+        visitor.pageview("/graphql/quote/" + index).send();
+        visitor.event("Return Quote", "Daily Quote").send()
+
+        var quote = flattenQuote( getQuote(index) );
+
+        return quote;
+
+    },
+    quote: function ( params ) {
+        var quote = {};
+
+        if ( typeof( quotes[ parseInt( params.id ) ] ) !== 'undefined' ) {
+            visitor.pageview("/graphql/quote/" + params.id).send();
+            visitor.event("Return Quote", "Fixed Quote").send()
+
+            quote = flattenQuote( getQuote( parseInt( params.id ) ) );
+        } else {
+            throw new Error( "Quote doesn't exist" );
+        }
+
+        return quote;
+    }
+};
 
 var server = restify.createServer( {
 
     name: 'howmuchisthe.fish',
     handleUpgrades: true,
-    version: '1.0.0'
+    version: '1.2.0'
 
 } );
 
 server.use( restify.plugins.queryParser() );
 server.use( restify.plugins.bodyParser() );
-//server.use( restify.CORS() );
-//server.use( restify.jsonp());
-//server.use( restify.gzipResponse());
-/*server.use( restify.throttle(
-    {
-        burst: 50,
-        rate: 30,
-        ip: true
-    }
-));*/
 
 server.get( {path: '/json'}, noRequest );
 server.get( {path: '/json/random'}, randomQuote );
@@ -38,6 +93,9 @@ server.get( {path: '/json/random/generate'}, generateQuote );
 server.get( {path: '/json/random/video'}, randomVideo );
 server.get( {path: '/json/daily'}, dailyQuote );
 server.get( {path: '/json/perma/:quoteId'}, fixedQuote );
+
+server.get( {path: '/graphql'}, noGql );
+server.post( {path: '/graphql'}, parseGql );
 
 server.get( {path: '/*'}, restify.plugins.serveStatic({
     directory: './htdocs',
@@ -168,6 +226,15 @@ function getQuote( index ) {
     return output;
 }
 
+function flattenQuote( obj ) {
+    var output = obj.quote;
+    output.id = obj.id;
+    output.permalink = obj.permalink;
+    output.hash = obj.hash;
+
+    return output;
+}
+
 function noRequest( req, res, next ) {
 
     res.setHeader('Access-Control-Allow-Origin','*');
@@ -179,32 +246,35 @@ function noRequest( req, res, next ) {
 
 }
 
-/*function setupNLP() {
+function noGql ( req, res, next ) {
+    res.setHeader('Access-Control-Allow-Origin','*');
 
-    var r, t = new Date().getTime();
+    var error = { 'error': 'Please POST GraphQL queries' };
 
-    r = setupNLPInner('text');
-    og = r.og; bg = r.bg;
+    res.send( 400, error );
+    return next();
+}
 
-    r = setupNLPInner('track');
-    ogt = r.og; bgt = r.bg;
+function parseGql( req, res, next ) {
+    res.setHeader('Access-Control-Allow-Origin','*');
 
-    r = setupNLPInner('album');
-    oga = r.og; bga = r.bg;
+    if ( req.body ) {
+        var response = {};
+        graphql(
+            gqlSchema,
+            req.body.query,
+            gqlImpl
+        ).then( result => {
+            res.send( 200, result );
 
-    var numtokens = og.length+bg.length+ogt.length+bgt.length+oga.length+bga.length;
-    console.log("setupNLP took: " + (new Date().getTime() - t) + "ms for " + numtokens + " tokens" );
+        });
 
-    if (testNLP == true) {
-
-        for ( var i = 0; i < 25; i++ ) {
-
-            console.log( generateString( og, bg, 13, 6 ) );
-
-        }
-
+    } else {
+        res.send( 400, { "error": "Bad request" } );
     }
-}*/
+
+    return next();
+}
 
 function annotateModel() {
     for ( var i = 0 ; i < quotes.length; i++ ) {
@@ -215,40 +285,6 @@ function annotateModel() {
         }
     }
 }
-
-/*function setupNLPInner( akey ) {
-
-    var corpora = "";
-
-    for ( var i = 0 ; i < quotes.length; i++ ) {
-
-        corpora = corpora + quotes[i][akey] + " ";
-
-    }
-
-    corpora = corpora.trim().toLowerCase().replace(/[^\w\s\'\.\,]/gim, '');
-
-    var aog = corpora.split(/[\s\,\.]+/);
-    var abg = [];
-
-    for (var i = 0, j = aog.length - 1; i < j; i++) {
-
-        // allow dot in right, comma in left.
-        if ( abg[i] != "." && abg[i + 1] != "," ) {
-
-            abg[i] = aog[i] + " " + aog[i + 1]
-
-        }
-
-    }
-
-    abg = abg.length ? abg : aog;
-
-    return { bg: abg, og: aog};
-
-}*/
-
-var rms;
 
 function setupNLP() {
     rms = {
@@ -315,65 +351,6 @@ function generateString( aKey ) {
 
     return tokenList;
 }
-
-/*function buildQuote() {
-
-    return  {
-                'quote': {
-                            'text': generateString( og, bg, 13, 6 ) + ".",
-                            'track': generateString( og.concat(ogt), bg.concat(bgt), 9, 4 ),
-                            'album': generateString( og.concat(oga), bg.concat(bga), 7, 4 ),
-                            'year': Math.floor(Math.random() * (new Date().getFullYear() - 1994 + 1)) + 1994
-                         }
-            };
-
-}
-
-function generateString( aog, abg, alenmin, alenmax ) {
-
-    var sWord = aog[Math.floor(Math.random() * aog.length-1) + 1];
-    var sLength = Math.floor(Math.random() * (alenmax - alenmin + 1)) + alenmin;
-    var sTokens = [];
-
-    var searchWord = sWord;
-
-    for (var i=0; i < sLength; i++) {
-
-        var tokenList = [];
-
-        for (var j = 0 ; j < abg.length; j++) {
-
-            var token = abg[j].split(" ");
-            if (token[0] == searchWord) {
-
-                tokenList.push(token);
-
-            }
-
-        }
-
-        var lToken = tokenList[Math.floor(Math.random() * tokenList.length-1) + 1];
-        sTokens.push(lToken[0]);
-        searchWord = lToken[1];
-
-        if (i == sLength - 1 && lToken[0].length < 4) {
-
-            sLength++;
-
-        }
-
-        // Quit if we encounter period.
-        if (searchWord == ".") {
-
-            break;
-
-        }
-
-    }
-
-    return sTokens.join(" ");
-
-}*/
 
 function getBasename( aQuoteIndex ) {
     return crypto.createHash('sha1').update(quotes[aQuoteIndex].year + "-" + quotes[aQuoteIndex].album + "-" + quotes[aQuoteIndex].track).digest('hex');
